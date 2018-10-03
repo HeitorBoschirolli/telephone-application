9e62862859409e2d0cb343f44777599b4ba3f018
HeitorBoschirolli <heitor.boschirolli@gmail.com>
2018-10-03 10:40:06 -0300

    lost calls are the only problem left

--- call_center.py
+++ call_center.py
@@ -1,71 +1,94 @@
 import cmd
 import argparse
+import Queue
 
 AVAILABLE = 1
 RINGING = 2
 BUSY = 3
+FOUND = True
 
 class CallCenter(cmd.Cmd):
 
     def do_call(self, id):
         print("Call", id, "received")
         cur_call = Call(id)
-        cur_call.search_operator(operators)
+        status = cur_call.search_operator()
+        if status == (not FOUND):
+            print("Call", id, "waiting in queue")
+            q.put(cur_call)
 
     def do_answer(self, id):
         operators[ord(id) - ord('A')].answer()
 
     
     def do_reject(self, id):
-        operators[ord(id) - ord('A')].answer()
+        operators[ord(id) - ord('A')].reject()
     
     def do_hangup(self, id):
-        print("you hanged up the call with id", id)
+        for operator in operators:
+            if operator.call != None:
+                if operator.call.id == id and operator.status == BUSY:
+                    operator.hangup()
+                    break
+                if operator.call.ic == id and operator.status == RINGING:
+                    print("Call", id, "missed")
+                    operator.hangup()
+                    break
 
     def do_EOF(self, line):
         return True
 
 class Operator:
-    def __init__(self, id, status=AVAILABLE, call_id=None):
+    def __init__(self, id, status=AVAILABLE, call=None):
         self.id = id
         self.status = status
-        self.call_id = call_id
-    def ring(self, call_id):
-        print ("Call", call_id, "ringing for operator", self.id)
+        self.call = call
+    def ring(self, call):
+        print ("Call", call.id, "ringing for operator", self.id)
         self.status = RINGING
-        self.call_id = call_id
+        self.call = call
     def answer(self):
-        print ("Call", self.call_id, "answered by operator", self.id)
+        print ("Call", self.call.id, "answered by operator", self.id)
         self.status = BUSY
     def reject(self):
-        print("Call", self.call_id, "rejected by operator", self.id)
+        print("Call", self.call.id, "rejected by operator", self.id)
         self.status = AVAILABLE
+        if self.call.search_operator() == (not FOUND):
+            print("Call", self.call.id, "waiting in queue")
+            q.put(self.call)
+        if not q.empty():
+            queue_call = q.get()
+            queue_call.search_operator()
+
     def hangup(self):
+        print("Call", self.call.id, "finished and operator", self.id, "available")
         self.status = AVAILABLE
+        if not q.empty():
+            queue_call = q.get()
+            queue_call.next_operator = ord(self.id) - ord('A')
+            queue_call.search_operator()
+
 
 class Call:
-    def __init__(self, id, last_operator=0):
+    def __init__(self, id, next_operator=0):
         self.id = id
-        self.last_operator = last_operator
-    def search_operator(self, operators):
-        self.last_operator = self.last_operator % len(operators)
-        for i in range(self.last_operator, len(operators)):
+        self.next_operator = next_operator
+    def search_operator(self):
+        i = self.next_operator
+        for i in range(self.next_operator, len(operators)):
             if operators[i].status == AVAILABLE:
-                operators[i].ring(self.id)
-                self.last_operator = i
-                break
+                operators[i].ring(self)
+                return FOUND
+            else:
+                self.next_operator = i + 1
+        return not FOUND
+            
 
 def create_operators(quantity=1):
     operators = list()
     for i in range(quantity):
         operators.append(Operator(chr(ord('A') + i)))
     return operators
-
-def look_for_operator(id, operators, last_seen):
-    for operator in operators:
-        if operator.status == AVAILABLE:
-            operator.ring(id)
-            break
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -79,5 +102,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     operators = create_operators(args.operators)
-
+    q = Queue.Queue()
     CallCenter().cmdloop()
